00cd9ce0a39e5ac1bbe60e423c4684dc15df11a4
Aashish Prasad <aashish.pd097@gmail.com>
2019-11-06 00:22:05 +0000

    Update wikiScrap.R

--- RScripts/wikiScrap.R
+++ RScripts/wikiScrap.R
@@ -8,7 +8,7 @@ wiki_url <- wiki_url[,-c(1)]
 
 for(i in 1:nrow(wiki_url)){
   print(i)
-  #i=3
+  #i=6
   url <- wiki_url[i,2]
   if(is.na(url)==FALSE){
     
@@ -17,7 +17,7 @@ for(i in 1:nrow(wiki_url)){
       url <- paste(url,"_(video_game)",sep = '')
     }
     page <- read_html(as.vector(url))
-    game_text <- page%>%html_nodes("h2+ p")%>%html_text()
+    game_text <- page%>%html_nodes("h2+ p,h3+p")%>%html_text()
     
     #text <- gsub("[\r\n]", "", game_text)
     tryCatch(
@@ -39,9 +39,9 @@ for(i in 1:nrow(wiki_url)){
         temp_df <- get_nrc_sentiment(text, cl = NULL, language = "english"),error = function(e){NA}
       )
     }
-    
+    #print(temp_df)
   }else{
-    print('in else') 
+    #print('in else') 
     temp_df <- data.frame('0','0','0','0','0','0','0','0','0','0')
     colnames(temp_df) <- c("anger","anticipation","disgust","fear","joy","sadness","surprise","trust","negative","positive")
   }
@@ -57,3 +57,40 @@ for(i in 1:nrow(wiki_url)){
 
 #merging sentiments with descriptive game data
 final_game_data <- cbind(game_table_distinct,sentiment_table)
+
+##########################
+#imputing missing values
+
+
+##########################
+
+#checking valid rows
+valid=0
+for(i in 1:nrow(sentiment_table)){
+  print(i)
+  flag=0
+  for(j in 1:10){
+    if(sentiment_table[i,j]!='0'){
+      flag=1
+    }
+  }
+  if(flag==1){
+    valid= valid+1
+    temp_df <- data.frame('1')
+    colnames(temp_df) <- c("valid")
+  }else{
+    temp_df <- NA
+  }
+  if(i>1){
+    dummy_df <- rbind(dummy_df,temp_df)
+  }else{
+    dummy_df <- temp_df
+  }
+}
+
+#removing rows with valid as NA
+final_game_data <- cbind(final_game_data,dummy_df)
+final_game_data <- final_game_data[complete.cases(final_game_data[ , 50]),]
+final_game_data <- final_game_data[,-c(50)]
+
+#view_table <- cbind(wiki_url,sentiment_table)
