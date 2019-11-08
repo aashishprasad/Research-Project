91a9125cf980d4d53df6d8a661776521ea750ee9
Aashish Prasad <aashish.pd097@gmail.com>
2019-11-08 23:58:00 +0000

    Update wikiScrap.R

--- RScripts/wikiScrap.R
+++ RScripts/wikiScrap.R
@@ -8,13 +8,15 @@ wiki_url <- wiki_url[,-c(1)]
 
 for(i in 1:nrow(wiki_url)){
   print(i)
-  #i=6
+  #i=33
   url <- wiki_url[i,2]
   if(is.na(url)==FALSE){
     
-    #fix for #25
+    #fix for #18 & #33
     if(i == 18){
       url <- paste(url,"_(video_game)",sep = '')
+    }else if(i == 33){
+      url<- "https://en.wikipedia.org/wiki/AO_Tennis_(video_game)"
     }
     page <- read_html(as.vector(url))
     game_text <- page%>%html_nodes("h2+ p,h3+p")%>%html_text()
@@ -57,10 +59,7 @@ for(i in 1:nrow(wiki_url)){
 
 #merging sentiments with descriptive game data
 final_game_data <- cbind(game_table_distinct,sentiment_table)
-
-##########################
-#imputing missing values
-
+final_game_data <- cbind(final_game_data,wiki_url$url)
 
 ##########################
 
@@ -88,9 +87,53 @@ for(i in 1:nrow(sentiment_table)){
   }
 }
 
-#removing rows with valid as NA
+#removing rows with 'valid' as NA
 final_game_data <- cbind(final_game_data,dummy_df)
-final_game_data <- final_game_data[complete.cases(final_game_data[ , 50]),]
-final_game_data <- final_game_data[,-c(50)]
+final_game_data <- final_game_data[complete.cases(final_game_data[ , 51]),]
+final_game_data <- final_game_data[,-c(51)]
 
+rownames(final_game_data) <- 1:nrow(final_game_data)#resetting row numbering
 #view_table <- cbind(wiki_url,sentiment_table)
+
+final_game_data <- data.frame(lapply(final_game_data, as.character), stringsAsFactors=FALSE)#converting factors to characters
+test_df <- data.frame(final_game_data)
+##########################
+#imputing missing values
+for(i in 1:nrow(final_game_data)){
+  print(i)
+  #flag=0
+  #i=40
+  url <- final_game_data[i,50]
+  #fix for #18 & #33
+  if(i == 18){
+    url <- paste(url,"_(video_game)",sep = '')
+  }else if(i == 33){
+    url<- "https://en.wikipedia.org/wiki/AO_Tennis_(video_game)"
+  }
+  page <- read_html(as.vector(url))
+  
+  for(j in 2:6){
+    if(is.na(final_game_data[i,j])||final_game_data[i,j]=='N/A'||final_game_data[i,j]=='TBA'||final_game_data[i,j]=='released'||final_game_data[i,j]=='N'){
+    #print('found')
+      if(j==2){
+        print(i)
+        game_text <- page%>%html_nodes("tr:nth-child(3) td")%>%html_text()
+        test_df[i,j] <- game_text[1]
+      }else if(j==3){
+        print(j)
+        game_text <- page%>%html_nodes("tr:nth-child(4) td")%>%html_text()
+      }else if(j==4){
+        print(j)
+        #game_text <- page%>%html_nodes("li:nth-child(1)")%>%html_text()
+      }else if(j==5){
+        print(j)
+        #game_text <- page%>%html_nodes("li:nth-child(1)")%>%html_text()
+      }else if(j==6){
+        print(j)
+        #game_text <- page%>%html_nodes("h2+ p,h3+p")%>%html_text()
+      }
+    }
+  }
+}
+
+
